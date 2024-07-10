06eb190ed1c4ec16422e5b2a4c082c7cc999554e
charlesinichi <ca.marquez@mail.clientstaff.co>
2024-07-10 15:04:32 +0800

    commit added design

---
+++ src/components/SignupInputs.jsx
@@ -0,0 +1,8 @@
+export default function SignupInputs({children, heading}){
+    return(
+        <div className="mt-8">
+            <h2 className="text-lg font-poppins indent-2 mb-2">{heading}</h2>
+            {children}
+        </div>
+    )
+}
