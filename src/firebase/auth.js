8de83a310279604fda95ad574d7da2eb44d15946
charlesinichi <ca.marquez@mail.clientstaff.co>
2024-07-15 10:50:46 +0800

    add google and facebook authentication

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -2,28 +2,37 @@ import { auth } from "./firebase";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-//   sendPasswordResetEmail,
-//   sendEmailVerification,
-//   updatePassword,
-//   signInWithPopup,
-//   GoogleAuthProvider,
+  signInWithPopup,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  //   sendPasswordResetEmail,
+  //   sendEmailVerification,
+  //   updatePassword,
 } from "firebase/auth";
 
 export const registerEmailPass = async (email, password) => {
   return createUserWithEmailAndPassword(auth, email, password);
 };
 
-export const signInEmailPass = (email, password) => {
+export const signInEmailPass = async (email, password) => {
   return signInWithEmailAndPassword(auth, email, password);
 };
 
-// export const doSignInWithGoogle = async () => {
-//   const provider = new GoogleAuthProvider();
-//   const result = await signInWithPopup(auth, provider);
-//   const user = result.user;
+export const doSignInWithGoogle = async () => {
+  const provider = new GoogleAuthProvider();
+  const result = await signInWithPopup(auth, provider);
+  const user = result.user;
 
-//   // add user to firestore
-// };
+  // add user to firestore
+};
+
+export const doSignInWithFacebook = async () => {
+  const provider = new FacebookAuthProvider();
+  const result = await signInWithPopup(auth, provider);
+  // const credential = FacebookAuthProvider.credentialFromResult(result);
+  // const accessToken = credential.accessToken; 
+  const user = result.user;
+}
 
 export const doSignOut = () => {
   return auth.signOut();
