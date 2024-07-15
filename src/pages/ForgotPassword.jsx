8418fd9ca95ce19ddc7ea47a311ea614b8d75b19
charlesinichi <ca.marquez@mail.clientstaff.co>
2024-07-15 20:38:52 +0800

    commit message

---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,89 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import SignupInputs from "../components/SignupInputs";
+import { toast, Toaster } from "sonner";
+import { auth } from "../firebase/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { doPasswordReset } from "../firebase/auth"; 
+import ArrowBack from "../assets/arrow-back.svg"
+function ForgotPassword() {
+  const navigate = useNavigate();
+  // const { userLoggedIn } = useAuth()
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate("/home");
+      } else {
+        console.log("no user");
+      }
+    });
+  });
+
+  const passwordReset = async () => {
+    try {
+      await doPasswordReset(email)
+        .then(() => {
+          toast.success("Password reset email sent", {
+            duration: 4000,
+            className: "bg-green-200",
+          });
+          setTimeout(() => {
+            navigate("/signin");
+          }, 4000);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          if (error) {
+            toast.warning(error.code, {
+              duration: 4000,
+              className: "bg-yellow-200",
+            });
+          }
+        });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <div className="flex md:columns-2">
+      <div className="w-full flex-col bg-food-pattern">
+        <div className="md:w-9/12 h-dvh content-center mx-14 xl:w-8/12 md:mx-auto">
+          <h1 className="font-poppins font-bold text-orangered text-3xl lg:text-5xl">
+            Forgot Password
+          </h1>
+          <p className="font-poppins mt-2 text-gray-500">No worries, we'll send you reset instructions</p>
+
+          <SignupInputs heading="Email">
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              name="email"
+              className="w-full border-y border-x border-x-slate-300 rounded-xl px-4 py-2 h-14 lg:h-16 font-poppins"
+              type="email"
+              required
+            />
+          </SignupInputs>
+
+          <button onClick={passwordReset} className="h-14 lg:h-16 mt-10 lg:text-xl text-white font-poppins rounded-xl w-full bg-orangered">
+            Continue
+          </button>
+          <div className="flex justify-center mt-10">
+            <img src={ArrowBack} height="30px" width="30px"/><Link to="/signin"><p className="text-gray-500 ms-2 mt-1 font-poppins font-medium text-sm lg:text-base" >Back To Login</p></Link>
+          </div>
+        </div>
+      </div>
+      <div className="grow text-wrap hidden md:flex md:bg-signup-pattern justify-end items-end bg-cover w-full">
+        <h1 className="stroke-black font-shadow text-right text-5xl mx-20 mb-10 w-4/5 font-bold text-white font-poppins">
+          Helping you plan your next meal filipino meal
+        </h1>
+      </div>
+      <Toaster expand visibleToasts={1} />
+    </div>
+  );
+}
+
+export default ForgotPassword;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -118,8 +118,8 @@ function Signup() {
   };
   return (
     <div className="flex md:columns-2">
-      <div className="w-full flex-col py-26 bg-food-pattern">
-        <div className="sm:w-9/12 mx-14 xl:w-8/12 sm:mx-auto">
+       <div className="w-full py-10 h-dvh content-center bg-food-pattern">
+        <div className="md:w-9/12 mx-14 xl:w-8/12 md:mx-auto">
           <div className="w-10 h-10 rounded-full bg-white flex justify-center items-center shadow-lg">
             <Link to={"/"}>
               <svg
@@ -241,7 +241,7 @@ function Signup() {
 
           <button
             onClick={register}
-            className="h-14 lg:h-16 mt-10 lg:text-xl text-white font-poppins rounded-xl w-full bg-orangered"
+            className="h-14 lg:h-16 mt-16 lg:text-xl text-white font-poppins rounded-xl w-full bg-orangered"
           >
             Sign Up
           </button>
@@ -267,10 +267,12 @@ function Signup() {
           </div>
         </div>
       </div>
-      <div className="grow text-wrap hidden md:flex md:bg-signup-pattern justify-end items-end bg-cover w-full">
-        <h1 className="stroke-black font-shadow text-right text-5xl mx-20 mb-10 w-4/5 font-bold text-white font-poppins">
-          Helping you plan your next meal filipino meal
-        </h1>
+      <div className="grow relative text-wrap hidden md:block md:bg-login-pattern bg-cover w-full">
+        <div className="absolute bottom-0 w-4/5 right-0">
+          <h1 className="stroke-black font-shadow text-right h-full text-5xl pb-5 pe-5 font-bold text-white font-poppins">
+            Helping you plan your next meal filipino meal
+          </h1>
+        </div>
       </div>
       <Toaster expand visibleToasts={1} />
     </div>
