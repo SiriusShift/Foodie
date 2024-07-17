864f9bfcab3c3adf909c755c0d6cb69850e1869e
charlesinichi <ca.marquez@mail.clientstaff.co>
2024-07-17 15:10:37 +0800

    fix button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return(
         <div className="w-full flex justify-between h-50 px-10 lg:px-20 py-3 md:py-4">
             <Link className="text-2xl md:text-4xl pt-1 md:pt-2 font-shrikhand" to={"/"}>Foodie</Link>
-            <Link to={"/signin"}><button className="bg-darrow font-poppins text-white font-bold text-xs lg:text-lg md:px-9 md:py-3 py-2 px-6 rounded-full" type="button">Sign In</button></Link>
+            <Link to={"/signin"}><button className="bg-darrow font-poppins text-white font-bold text-xs lg:text-lg md:px-9 md:py-3 py-3 px-5 rounded-full" type="button">Sign In</button></Link>
         </div>
     )
 }
